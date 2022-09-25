89be8059b6e313514566f858353c7515e616efd3
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2022-09-25 13:14:43 +1000

    initial guess

---
+++ bnr1/bnr1.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "quantum.h"
+
+#define XXX KC_NO
+
+#define LAYOUT_all( \
+	k0A \
+) { \
+	{k0A} \
+}
---
+++ bnr1/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x1B, 0x8A, 0x9D, 0xEE, 0x89, 0x9B, 0xEA, 0x46}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0}
+#define VIAL_UNLOCK_COMBO_COLS {0}
